0dbaf42478148278b6f6c077d8d69b694c45a02e
ufbfung <ufbfung@gmail.com>
2025-12-18 10:55:48 -0500

    add supported resource to index export

--- src/index.ts
+++ src/index.ts
@@ -6,3 +6,6 @@ export * from "./r4/resources/Observation";
 export * from "./r4/resources/Condition";
 export * from "./r4/resources/AllergyIntolerance";
 export * from "./r4/resources/Practitioner";
+
+// Scope types to only what's supported to incrementally build this package
+export * from "./r4/supported";
